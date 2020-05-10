c6666339f647a16501e971ce027e0451f67ca328
Sameer Kolhar <kolhar730@gmail.com>
2020-05-11 02:09:55 +0530

    Update core.clj with evaluation

--- src/kiddo/core.clj
+++ src/kiddo/core.clj
@@ -29,7 +29,7 @@
   [repl-options]
   (apply main/repl repl-options))
 
-(REPL repl-options)
+;; (REPL repl-options)
 
 (defn is-lowercase?
   "Returns true if word is in lowercase"
@@ -68,7 +68,8 @@
   [word]
   (is-valid-token? #"[A-Za-z]+$" word))
 
-;; FIXME: Check the regex of the symbol - it is incorrect for certain inputs
+;; FIXME: Check the regex of the symbol - it is incorrect for certain inputs like
+;; asdasd12asda
 
 (defn- get-number-value
   [word]
@@ -83,15 +84,62 @@
   (let [trim-str (str/trim input)
         cut-whitespaces (str/split trim-str #" ")
         check-if-lowercase (every? is-lowercase? cut-whitespaces)]
-    (if (not check-if-lowercase)
+    (if-not check-if-lowercase
       (throw (Exception. "Ooh! Something's fishy!"))
-      ;; Now's where the fun begins - tokens are set into
+      ;; Now's where the fun begins - tokens are set into a flat list
       (mapv (fn [val]
               (cond
                 (is-reserved? val) {:type "reserved" :value val}
-                (is-number? val) {:type "number" :value val}
+                (is-number? val) {:type "number" :value (Integer/parseInt val 10)}
                 (is-string? val) {:type "string" :value val}
                 (is-symbol? val) {:type "symbol" :value val}))
             cut-whitespaces))))
 
 (tokenize "kiddo is \"language\"")
+(tokenize "alter is 23")
+
+(def var-store
+  "The main store for storing variables and other values"
+  (atom {}))
+
+(defn add-to-store
+  "Appends a new value to the var-store"
+  [state name value]
+  (swap! state assoc name value))
+
+;; (add-to-store var-store "kiddo" "language")
+
+(defn get-instruction
+  "Returns the first token that is reserved"
+  [tokens-coll]
+  (let [instr (nth (filter #(= "reserved" (:type %)) tokens-coll) 0)
+        operands (filter #(not= instr %) tokens-coll)]
+    [instr operands]))
+
+(declare exec-instruction)
+
+(defn exec-is
+  "Executes the `is` method"
+  [operands]
+  ;; Make a helper called get value common for all the instructions
+  (let [key (:value (some #(when (= "symbol" (:type %)) %) operands))
+        value (:value (some #(when (not= "symbol" (:type %)) %) operands))]
+    (add-to-store var-store key value)))
+
+(defn exec-instruction
+  "Returns the value obtained from executing an instruction"
+  [[instr-map operands]]
+  (let [instr (:value instr-map)]
+    ;; FIXME: Replace with a better way to choose btw instructions
+    (when (= instr "is")
+      (exec-is operands))))
+
+;; Execution of the sample program from tokenization to
+;; performing the action
+
+(-> (tokenize "kio is 42")
+    (get-instruction)
+    (exec-instruction))
+
+;; Maybe keep this store permanent
+@var-store
